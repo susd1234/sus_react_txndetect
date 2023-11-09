6574c0a23e81d28bf6764b869fe618bb89411233
Susan Das <susandas@gmail.com>
2023-11-09 01:00:32 +0000

    added style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Papa from "papaparse";
 
-// import { Tab } from "bootstrap";
+const apiPOSTEndpoint = process.env.REACT_APP_API_ENDPOINT_POST;
+const apiGETEndpoint = process.env.REACT_APP_API_ENDPOINT_GET;
 
 class App extends Component {
   state = {
@@ -25,16 +26,8 @@ class App extends Component {
         this.state.selectedFile.name
       );
 
-      // Fixed the axios request by properly handling the response and error.
       axios
-        .post(
-          // "https://md4mypmtea.execute-api.ap-south-1.amazonaws.com/v1/genai-credit-fraud-detection/input_txn.csv",
-          // "https://1wjikouds9.execute-api.ap-south-1.amazonaws.com/v1/s3?key=genai-credit-fraud-detection/Sample_Input_File.csv",
-          "https://jenl9gn7dl.execute-api.ap-south-1.amazonaws.com/s3-upld",
-          // "https://5e0b4c1bxf.execute-api.ap-south-1.amazonaws.com/GenAI-CreditFraud-lambda1",
-          // "https://1wjikouds9.execute-api.ap-south-1.amazonaws.com/v6/s3?key=genai-credit-fraud-detection/put_test_3.csv",
-          formData
-        )
+        .post(apiPOSTEndpoint, formData)
         .then((response) => {
           // Handle the successful upload here
           this.setState({ selectedFile: null });
@@ -44,19 +37,6 @@ class App extends Component {
           // Handle the error here
           console.error("File upload error:", error);
         });
-
-      // const [records, setRecords] = useState([]);
-      // axios
-      //   .get(
-      //     "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-      //   )
-      //   .then((res) => {
-      //     setRecords(res.data);
-      //   })
-      //   .catch((error) => {
-      //     // Handle the error here
-      //     console.error("File Fetch Error:", error);
-      //   });
     }
   };
 
@@ -79,14 +59,15 @@ class App extends Component {
       return (
         <div>
           <br />
-          <h4>File has been Successfully Uploaded!!</h4>
+          <br />
+          {/* <h4>File has been Successfully Validated by AI!!</h4> */}
         </div>
       );
     } else {
       return (
         <div>
           <br />
-          <h4>Choose a File and then Press the Upload button</h4>
+          <h4>Choose the Txn File and then Press the Validate button</h4>
         </div>
       );
     }
@@ -98,9 +79,7 @@ class App extends Component {
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await axios.get(
-            "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-          );
+          const response = await axios.get(apiGETEndpoint);
           const parsedData = Papa.parse(response.data, { header: true });
 
           setData(parsedData.data);
@@ -133,89 +112,15 @@ class App extends Component {
     );
   };
 
-  // FetchData = () => {
-  //   const [records, setRecords] = useState([]);
-
-  //   useEffect = () => {
-  //     axios
-  //       .get(
-  //         "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-  //       )
-  //       .then((res) => {
-  //         setRecords(res.data);
-  //       })
-  //       .catch((error) => {
-  //         // Handle the error here
-  //         console.error("File Fetch Error:", error);
-  //       });
-  //   };
-  //   return (
-  //     <div>
-  //       <table>
-  //         <tbody>
-  //           {records.map((r, i) => (
-  //             <tr key={i}>
-  //               <th>ID</th>
-  //               <td>{r.id}</td>
-  //               <th>Name</th>
-  //               <td>{r.name}</td>
-  //             </tr>
-  //           ))}
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // };
-
-  // FetchData = () => {
-  //   const [records, setRecords] = useState([]);
-
-  //   useEffect(() => {
-  //     axios
-  //       .get(
-  //         "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-  //       )
-  //       .then((res) => {
-  //         setRecords(res.data);
-  //       })
-  //       .catch((error) => {
-  //         // Handle the error here
-  //         console.error("File Fetch Error:", error);
-  //       });
-  //   }, []);
-
-  //   return (
-  //     <div>
-  //       {records}
-  //       <table>
-  //         <tbody>
-  //           {records.map((r, i) => (
-  //             <tr key={i}>
-  //               <th>ID</th>
-  //               <td>{r.id}</td>
-  //               <th>Name</th>
-  //               <td>{r.name}</td>
-  //             </tr>
-  //           ))}
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // };
-
-  // export default FetchData;
-
-  //     .catch((err) => console.log(err));
-  // }, [])
-
   render() {
     return (
-      <div className="container">
-        <h2>File Upload</h2>
-        <h3>File Upload with React and AWS Serverless APIs</h3>
-        <div>
+      <div id="container">
+        <h2>Please Upload the Transation File (.csv) for AI Validation</h2>
+        <br />
+        <br />
+        <div id="upldbutton">
           <input type="file" onChange={this.onFileChange} />
-          <button onClick={this.onFileUpload}>Upload</button>
+          <button onClick={this.onFileUpload}>Validate</button>
           {this.state.fileUploadedSuccessfully && <FetchData />}
         </div>
         {this.fileData()}
@@ -231,9 +136,6 @@ class App extends Component {
 
 export default App;
 
-// write a function to GET the file from S3 and display it in the browser
-//
-
 function FetchData() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true); // Initial loading state
@@ -243,7 +145,8 @@ function FetchData() {
     // Simulate a 5-second wait
     const timer = setTimeout(() => {
       setLoading(false); // Set loading to false after 5 seconds
-      setContent("Content to display after 5 seconds");
+      // setContent("Content to display after 5 seconds");
+      setContent("Txns have been Successfully Validated by AI !!");
     }, 5000); // 5000 milliseconds = 5 seconds
 
     // Clear the timer when the component unmounts to avoid memory leaks
@@ -255,9 +158,7 @@ function FetchData() {
       try {
         // Simulate a 5-second wait before making the request
         await new Promise((resolve) => setTimeout(resolve, 5000));
-        const response = await axios.get(
-          "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-        );
+        const response = await axios.get(apiGETEndpoint);
         const parsedData = Papa.parse(response.data, { header: true });
 
         setData(parsedData.data);
@@ -292,53 +193,17 @@ function FetchData() {
               ))}
             </tbody>
           </table>
-          {content}
+          {/* {content} */}
+          <span id="tbl">
+            <br />
+            <br />
+            {content}
+            <br />
+            <br />
+            {/* <h4>File has been Successfully Validated by AI!!</h4> */}
+          </span>
         </div>
       )}
     </div>
   );
 }
-
-// function FetchData() {
-//   const [records, setRecords] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(
-//           "https://kxktg7txaf.execute-api.ap-south-1.amazonaws.com/lambda-s3-get"
-//         );
-//         setRecords(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <br />
-//             <th>Name</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {records[(0, 10)]}
-//           {/* {records.map((record, i) => (
-//           <tr key={i}>
-//             <td>{record.id}</td>
-//             <td>{record.name}</td>
-//           </tr>
-//           ))} */}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default FetchData;
